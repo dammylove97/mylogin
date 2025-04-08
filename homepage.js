1fe283158cfe1b47745ee8c3d582a49f6742c88b
Yusuf Oluwatoyin <yusuf-oluwatoyin@scarlet.lan>
2025-04-08 15:26:30 +0100

    updated

---
+++ homepage.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+if (!currentUser) {
+    window.location.href = 'login.html';
+} else {
+
+    //Display username on the dashboard
+    const welcomeElement = document.getElementById('welcomeUser');
+    if (welcomeElement) {
+        welcomeElement.textContent = currentUser.username;
+    }
+
+    //logout button
+    const logoutBtn = document.getElementById('logoutBtn');
+    if (logoutBtn) {
+        logoutBtn.addEventListener('click', function() {
+
+            //clear user
+            localStorage.removeItem('currentUser');
+
+            //Now after removing goto login page
+            window.location.href = 'login.html';
+        });
+    }
+}
+
+});
